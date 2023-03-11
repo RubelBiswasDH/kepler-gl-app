9669e9c049fdd9794ee49e24efcb6cc69514e822
RubelBiswasDH <rubelbiswas.dh@gmail.com>
2023-03-11 23:39:45 +0600

    First commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import keplerGlReducer from "kepler.gl/reducers";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { taskMiddleware } from "react-palm/tasks";
+import { Provider } from "react-redux";
+import KeplerGl from "kepler.gl";
+
+const reducers = combineReducers({
+  keplerGl: keplerGlReducer
+});
+
+const store = createStore(reducers, {}, applyMiddleware(taskMiddleware));
+
+export default function App() {
+  return (
+    <Provider store={store}>
+      <Map />
+    </Provider>
+  );
+}
+
+function Map() {
+
+  return (
+    <KeplerGl
+      id="map"
+      mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_API}
+      width={window.innerWidth}
+      height={window.innerHeight}
+    />
+  );
+}
